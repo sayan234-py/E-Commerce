ff0ca3fa4e9cedacf1631d7921116ba66b047a8a
sayan234-py <nsayan520@gmail.com>
2026-02-26 23:15:30 +0530

    Refactor Item component price display elements

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,17 +4,14 @@ import "./Item.css";
 
 const Item = ({ id, image, name, new_price, old_price }) => {
   return (
-    <Link
-      to={`/product/${id}`}
-      className="item-link"
-      onClick={() => window.scrollTo(0, 0)}
-    >
+    <Link to={`/product/${id}`} className="item-link">
       <div className="item">
         <img src={image} alt={name} />
         <p>{name}</p>
+
         <div className="item-prices">
-          <div className="item-price-new">₹{new_price}</div>
-          <div className="item-price-old">₹{old_price}</div>
+          <span className="item-price-new">₹{new_price}</span>
+          <span className="item-price-old">₹{old_price}</span>
         </div>
       </div>
     </Link>
