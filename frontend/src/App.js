e207d5b9246d34ad77c21b05fa2eddfd56c716e6
sayan234-py <nsayan520@gmail.com>
2026-03-01 14:05:58 +0530

    Refactor App.js for cleaner routing structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -9,31 +8,30 @@ import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
 
-const men_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737681/banner_mens_kw6tph.png"
-const women_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737026/banner_women_vtvhc1.png"
-const kid_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737682/banner_kids_abxmkm.png"
+const men_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737681/banner_mens_kw6tph.png";
+const women_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737026/banner_women_vtvhc1.png";
+const kid_banner = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737682/banner_kids_abxmkm.png";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path='/' element={<Shop />} />
         <Route path='/mens' element={<ShopCategory category="men" banner={men_banner} />} />
         <Route path='/womens' element={<ShopCategory category="women" banner={women_banner} />} />
         <Route path='/kids' element={<ShopCategory category="kid" banner={kid_banner} />} />
-        <Route path='/product' element={<Product/>}>
-        <Route path=':productId' element={<Product/>} />
-        </Route>
-        
+
+        {/* FIXED ROUTE */}
+        <Route path="/product/:productId" element={<Product />} />
+
         <Route path='/login' element={<LoginSignup />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
+
       <Footer />
-      </BrowserRouter>
-      
-    </div>
+    </BrowserRouter>
   );
 }
 
