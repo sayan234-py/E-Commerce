ac27aaa1414602aa1b594ba8c4b2453dfa44a2bf
sayan234-py <nsayan520@gmail.com>
2026-04-06 15:02:11 +0530

    Enhance Item component to display old price
    Added old_price prop to Item component and updated layout.

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Item.css";
 
-const Item = ({ id, image, name, new_price }) => {
+const Item = ({ id, image, name, new_price, old_price }) => {
   return (
     <Link
       to={`/product/${id}`}
-      onClick={() => window.scrollTo(0, 0)}
+      className="item-link"
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
     >
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>₹{new_price}</p>
+      <div className="item">
+        <div className="item-img-container">
+          <img src={image} alt={name} />
+        </div>
+
+        <div className="item-details">
+          <p className="item-name">{name}</p>
+
+          <div className="item-prices">
+            <span className="item-price-new">₹{new_price}</span>
+            {old_price && (
+              <span className="item-price-old">₹{old_price}</span>
+            )}
+          </div>
+        </div>
       </div>
     </Link>
   );
