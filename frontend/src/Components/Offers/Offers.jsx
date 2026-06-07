12fd75ede68216036f86a50847872b3a24f893d7
Sayan Nandi <nsayan520@gmail.com>
2026-06-07 17:19:55 +0530

    Refactor Offers component layout and image source

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import './Offers.css'
-// import excluve_image from '../Assets/exclusive_image.png'
+import React from 'react';
+import './Offers.css';
 
-const exclusive_image = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770738107/exclusive_image_kmgre2.png"
+// Your original asset (commented out just in case you need it later)
+// const exclusive_image_original = "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770738107/exclusive_image_kmgre2.png";
+
+// A curated Y2K street style asset that matches the brutalist aesthetic perfectly
+const exclusive_image = "https://images.unsplash.com/photo-1617317376997-8748e6862c01?auto=format&fit=crop&w=800&h=800&q=80";
 
 const Offers = () => {
   return (
-    <div className='offers'>
-        <div className="offers-left">
-            <h1>Exclusive</h1>
-            <h1>Offers for you</h1>
-            <p>only on best sellers product</p>
-            <button>Check Now</button>
+    <div className="genz-offers">
+      <div className="offers-grid">
+        
+        {/* Left Side: Typography & Interactions */}
+        <div className="offers-content">
+          <div className="offers-badge">! RARE FIND</div>
+          
+          <h1 className="offers-title">
+            <span className="hollow">EXCLUSIVE</span>
+            <div className="solid">OFFERS FOR YOU.</div>
+          </h1>
+          
+          <p className="offers-desc">* ONLY ON BEST SELLERS PRODUCT *</p>
+          
+          <button className="btn-offers">
+            CHECK NOW ⚡
+          </button>
         </div>
-        <div className="offers-right">
-            <img src={exclusive_image} alt="" />
+
+        {/* Right Side: Retro Polaroid Frame */}
+        <div className="offers-image-panel">
+          <div className="polaroid-container">
+            <div className="tape"></div>
+            <img src={exclusive_image} alt="Exclusive Offer" />
+            <div className="polaroid-caption">ARCHIVE_LOOT.JPG</div>
+          </div>
         </div>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Offers;
