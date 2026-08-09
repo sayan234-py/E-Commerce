58397a2c67772c696e9cc4e65d45e60e0401f423
Sayan Nandi <nsayan520@gmail.com>
2026-08-09 14:34:11 +0530

    Add BannerCarousel and update ShopCategory component

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -2,8 +2,10 @@ import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext.jsx";
 import Item from "../Components/Item/Item.jsx";
 import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import BannerCarousel from "../Components/BannerCarousel/BannerCarousel";
 import "./CSS/ShopCategory.css";
-const ShopCategory = ({ banner, category }) => {
+
+const ShopCategory = ({ banners = [], category }) => {
   const { all_product } = useContext(ShopContext);
   const filteredProducts = all_product.filter(
     (item) => item.category === category
@@ -11,7 +13,9 @@ const ShopCategory = ({ banner, category }) => {
 
   return (
     <div className="shop-category">
-      <img src={banner} alt="" className="shopcategory-banner" />
+      {/* Auto-sliding Banner with Dots */}
+      <BannerCarousel banners={banners} />
+
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of {filteredProducts.length} products
@@ -20,20 +24,20 @@ const ShopCategory = ({ banner, category }) => {
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
-
       <div className="shopcategory-products">
         {filteredProducts.map((item, i) => (
-            <Item
-              key={i}
-              id={item.id}
-              image={item.image}
-              name={item.name}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          ))}
+          <Item
+            key={i}
+            id={item.id}
+            image={item.image}
+            name={item.name}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   );
 };
+
 export default ShopCategory;
