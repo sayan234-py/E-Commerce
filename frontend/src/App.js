d9c7014bfaf0e9caf98fd17ba70b3caa0c50124e
Sayan Nandi <nsayan520@gmail.com>
2026-08-09 15:22:40 +0530

    Add new banner images for men's, women's, and kids' categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,22 +12,22 @@ import PopupChatbot from './Components/PopupChatbot/PopupChatbot';
 // Men's Category Banners
 const men_banners = [
   "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737681/banner_mens_kw6tph.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737681/banner_mens_secondary.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737681/banner_mens_tertiary.png"
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269044/Screenshot_2026-08-09_at_3.12.54_PM_oufnuw.png",
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269044/Screenshot_2026-08-09_at_3.13.12_PM_w4cfgl.png"
 ];
 
 // Women's Category Banners
 const women_banners = [
   "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737026/banner_women_vtvhc1.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737026/banner_women_secondary.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737026/banner_women_tertiary.png"
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269044/Screenshot_2026-08-09_at_3.14.01_PM_fk65ee.png",
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269044/Screenshot_2026-08-09_at_3.16.42_PM_fifeoi.png"
 ];
 
 // Kids Category Banners
 const kid_banners = [
   "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737682/banner_kids_abxmkm.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737682/banner_kids_secondary.png",
-  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1770737682/banner_kids_tertiary.png"
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269056/Screenshot_2026-08-09_at_3.17.39_PM_p2sqkw.png",
+  "https://res.cloudinary.com/dp2h1zqb4/image/upload/v1786269057/Screenshot_2026-08-09_at_3.19.34_PM_jh5gy4.png"
 ];
 
 function App() {
